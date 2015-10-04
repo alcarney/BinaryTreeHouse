42c7e55cb370a136425b0b63ac8faa6571b6cbe6
Alex Carney <alcarneyorg@gmail.com>
2015-10-04 16:10:53 +0100

    BinaryTreeHouse now an instance of Bifunctor
    The ~binTreeLeafMap~ and ~binTreeBranchMap~ functions have been renamed
    to ~first~ and ~second~ respectively and moved to form the instance
    declaration of Bifunctor for this data strucutre

--- src/Data/BinaryTreeHouse.hs
+++ src/Data/BinaryTreeHouse.hs
@@ -2,14 +2,11 @@ module Data.BinaryTreeHouse
 (
     BinaryTreeHouse(..),
 
-    binTreeMap,
-    binTreeLeafMap,
-    binTreeBranchMap,
-
     growTree
 ) where
 
 import Data.Block
+import Data.Bifunctor
 
 data BinaryTreeHouse a b = Nil
                          | Leaf a
@@ -24,20 +21,16 @@ showBinaryTreeHouse (Branch h x y) = addToTheRight 0 (addToTheRight 0 left house
   where house = showBox 0 h
         left = padTop (height house) (showBinaryTreeHouse x)
         right = padTop (height house) (showBinaryTreeHouse y)
-binTreeMap :: (a -> b) -> BinaryTreeHouse a a -> BinaryTreeHouse bb
-binTreeMap _ Nil = Nil
-binTreeMap f (Leaf a) = Leaf (f a)
-binTreeMap f (Branch house left right) = Branch (f house) (binTreeMap f left) (binTreeMap f right)
+instance Bifunctor BinaryTreeHouse where
 
-binTreeLeafMap :: (a -> b) -> BinaryTreeHouse a c -> BinaryTreeHouse b c
-binTreeLeafMap _ Nil = Nil
-binTreeLeafMap f (Leaf a) = Leaf (f a)
-binTreeLeafMap f (Branch house left right) = Branch house (binTreeLeafMap f left) (binTreeLeafMap f right)
+  first _ Nil = Nil
+  first f (Leaf a) = Leaf (f a)
+  first f (Branch house left right) = Branch house (first f left) (first f right)
 
-binTreeBranchMap :: (b -> c) -> BinaryTreeHouse a b -> BinaryTreeHouse a c
-binTreeBranchMap _ Nil = Nil
-binTreeBranchMap _ (Leaf a) = Leaf a
-binTreeBranchMap f (Branch house left right) = Branch (f house) (binTreeBranchMap f left) (binTreeBranchMap right)
+  second :: (b -> c) -> BinaryTreeHouse a b -> BinaryTreeHouse a c
+  second _ Nil = Nil
+  second _ (Leaf a) = Leaf a
+  second f (Branch house left right) = Branch (f house) (second f left) (second right)
 growTree :: b -> BinaryTreeHouse a b -> BinaryTreeHouse a b -> BinaryTreeHouse a b
 growTree _ Nil Nil = Nil
 growTree house left right = Branch house left right
